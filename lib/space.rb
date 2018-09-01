8b1714afc10d3b826570f88e204a9e385bad715a
Bailey Diveley <bailey.diveley@gmail.com>
2018-09-01 17:08:52 -0600

    Add pegs and ships to space class and test" exit

--- lib/space.rb
+++ lib/space.rb
@@ -3,11 +3,35 @@ class Space
 
   def initialize(coordinate)
     @coordinate = coordinate
+    @has_ship = false
+    @has_peg = false
   end
 
-  def has_ship?(ship)
-    ship.coordinates.any? do |coord|
-      coord == @coordinate
+  def has_ship?
+      @has_ship
+  end
+
+  def add_ship(ship)
+    if @has_ship == false
+      if ship.coordinates.any? do |coord|
+        coord == @coordinate
+        @has_ship = true
+        end
+      end
+    else
+      "Can't do that"
+    end
+  end
+
+  def has_peg?
+      @has_peg
+  end
+
+  def add_peg(peg)
+    if @has_peg == false && @coordinate == peg.guess
+      @has_peg = true
+    else
+      "Can't do that"
     end
   end
 
--- test/space_test.rb
+++ test/space_test.rb
@@ -14,22 +14,74 @@ class SpaceTest < Minitest::Test
     assert_equal 'A1', space.coordinate
   end
 
-  def test_space_has_a_ship?
+  def test_space_starts_with_no_ship
     space_1 = Space.new('A1')
-    space_2 = Space.new('A2')
-    ship = Ship.new("Destroyer", 2)
-    ship.assign_coordinates('A1', 'A2')
 
-    assert space_1.has_ship?(ship)
-    assert space_2.has_ship?(ship)
+    refute space_1.has_ship?
   end
 
-  def test_space_has_no_ship?
+  def test_can_add_ship_to_space_if_coords_match
     space = Space.new('A1')
     ship = Ship.new("Destroyer", 2)
-    ship.assign_coordinates('A2', 'B2')
+    ship.assign_coordinates('A1', 'B2')
+    space.add_ship(ship)
 
-    refute space.has_ship?(ship)
+    assert space.has_ship?
+  end
+
+  def test_cannot_add_ship_to_space_if_coords_dont_match
+    space = Space.new('A1')
+    ship = Ship.new("Destroyer", 2)
+    ship.assign_coordinates('B1', 'B2')
+    space.add_ship(ship)
+
+    assert space.has_ship?
+  end
+
+  def test_cannot_add_more_than_one_ship_to_same_space
+    space = Space.new('A1')
+    ship_1 = Ship.new("Destroyer", 2)
+    ship_1.assign_coordinates('B1', 'B2')
+    ship_2 = Ship.new("Cruiser", 3)
+    ship_2.assign_coordinates('B2', 'B4')
+
+    space.add_ship(ship_1)
+    space.has_ship?
+
+    assert_equal "Can't do that",     space.add_ship(ship_2)
+  end
+
+  def test_space_starts_with_no_peg
+    space_1 = Space.new('A1')
+
+    refute space_1.has_peg?
+  end
+
+  def test_can_add_peg_to_space_if_coords_match
+    space = Space.new('A1')
+    peg = Peg.new('A1')
+    space.add_peg(peg)
+
+    assert space.has_peg?
+  end
+
+  def test_cannot_add_peg_to_space_if_coords_dont_match
+    space = Space.new('A1')
+    peg = Peg.new('B1')
+    space.add_peg(peg)
+
+    refute space.has_peg?
+  end
+
+  def test_cannot_add_more_than_one_peg_to_same_space
+    space = Space.new('A1')
+    peg_1 = Peg.new('A1')
+    peg_2 = Peg.new('A1')
+
+    space.add_peg(peg_1)
+    space.has_peg?
+
+    assert_equal "Can't do that",     space.add_peg(peg_2)
   end
 
 end
