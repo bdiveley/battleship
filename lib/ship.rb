35df5849f9b4bdec40db5bef9db2b26f3629fb53
Bailey Diveley <bailey.diveley@gmail.com>
2018-09-01 14:26:56 -0600

    add assign_coordinates methods to ship class and test

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,9 +1,51 @@
 class Ship
     attr_reader :name,
-                :length
+                :length,
+                :coordinates
 
   def initialize(name, length)
     @name = name
     @length = length
+    @coordinates = []
   end
+
+  def assign_coordinates(coord_1, coord_2)
+    @coordinates << coord_1
+    if coord_1.chars[-1].to_i + 1 == coord_2[-1].to_i ||
+      num_coord(coord_1.chars[0]) + 1 == num_coord(coord_2.chars[0])
+      @coordinates << coord_2
+    elsif
+      coord_1.chars[0] == coord_2.chars[0]
+      @coordinates << coord_1[0] + (coord_1[-1].to_i + 1).to_s
+      @coordinates << coord_2
+    else
+      @coordinates << alpha_coord(num_coord(coord_1[0]) + 1) + coord_1[-1]
+      @coordinates << coord_2
+    end
+  end
+
+  def num_coord(coord)
+     if coord == "A"
+       1
+     elsif coord == "B"
+       2
+     elsif coord == "C"
+       3
+     elsif coord == 'D'
+       4
+     end
+  end
+
+  def alpha_coord(coord)
+    if coord == 1
+      "A"
+    elsif coord == 2
+      "B"
+    elsif coord == 3
+      "C"
+    elsif coord == 4
+      "D"
+    end
+  end
+
 end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -19,4 +19,59 @@ class ShipTest < Minitest::Test
     assert_equal 3, ship.length
   end
 
+  def test_ship_starts_with_no_coordinates
+    ship = Ship.new("Cruiser", 3)
+
+    assert_equal [], ship.coordinates
+  end
+
+  def test_destroyer_can_be_assigned_coordinates_horizontally
+    ship = Ship.new("Destroyer", 2)
+
+    expected = ['A1', 'A2']
+    actual = ship.assign_coordinates('A1', 'A2')
+    assert_equal expected, actual
+  end
+
+  def test_cruiser_can_be_assigned_coordinates_horizontally
+    ship = Ship.new("Cruiser", 3)
+
+    expected = ['A1', 'A2', 'A3']
+    actual = ship.assign_coordinates('A1', 'A3')
+    assert_equal expected, actual
+  end
+
+  def test_destroy_can_be_assigned_coordinates_vertically
+    ship = Ship.new("Destroyer", 2)
+
+    expected = ['A1', 'B1']
+    actual = ship.assign_coordinates('A1', 'B1')
+    assert_equal expected, actual
+  end
+
+  def test_cruiser_can_be_assigned_coordinates_vertically
+    ship = Ship.new("Cruiser", 3)
+
+    expected = ['A1', 'B1', 'C1']
+    actual = ship.assign_coordinates('A1', 'C1')
+    assert_equal expected, actual
+  end
+
+  def test_alpha_coordinates_changed_to_nums
+    ship = Ship.new("Cruiser", 3)
+
+    assert_equal 1, ship.num_coord('A')
+    assert_equal 2, ship.num_coord('B')
+    assert_equal 3, ship.num_coord('C')
+    assert_equal 4, ship.num_coord('D')
+  end
+
+  def test_num_coordinates_changed_to_alpha
+    ship = Ship.new("Cruiser", 3)
+
+    assert_equal 'A', ship.alpha_coord(1)
+    assert_equal 'B', ship.alpha_coord(2)
+    assert_equal 'C', ship.alpha_coord(3)
+    assert_equal 'D', ship.alpha_coord(4)
+  end
 end
