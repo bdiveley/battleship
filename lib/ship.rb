91cf6909a960394e4c4a741f7c69141021e5e37d
Bailey Diveley <bailey.diveley@gmail.com>
2018-09-01 12:47:39 -0600

    finish Ship class

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,3 +1,9 @@
 class Ship
+    attr_reader :name,
+                :length
 
-end 
+  def initialize(name, length)
+    @name = name
+    @length = length
+  end
+end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -6,5 +6,17 @@ require 'pry'
 
 class ShipTest < Minitest::Test
 
+  def test_new_ship_exists
+    ship = Ship.new("Destroyer", 2)
+
+    assert_instance_of Ship, ship
+  end
+
+  def test_instance_variables
+    ship = Ship.new("Cruiser", 3)
+
+    assert_equal "Cruiser", ship.name
+    assert_equal 3, ship.length
+  end
 
 end
